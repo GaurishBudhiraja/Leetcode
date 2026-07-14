7292b8bf8a249e5f003067de21a248364e12effd
Gaurish Budhiraja <gaurishbudhiraja2005@gmail.com>
2026-07-14 12:00:53 +0530

    Time: 6 ms (16.1%), Space: 15.2 MB (44.55%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    static bool cmp(pair<int,int> a, pair<int,int> b) {
-        if(a.second == b.second){
-            return a.first > b.first;
-        }
-        return a.second<b.second;
-    }
-    vector<int> frequencySort(vector<int>& nums){
-        unordered_map<int,int>mp;
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int> mp;
         for(int num : nums){
             mp[num]++;
         }
-        vector<pair<int,int>> v;
-        for(auto &it : mp){
-            v.push_back({it.first, it.second});
-        }
-        sort(v.begin(), v.end(), cmp);
-        vector<int> res;
-        for(auto &it : v){
-            while(it.second > 0){
-                res.push_back(it.first);
-                it.second--;
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+            if(mp[a] == mp[b]){
+                return a > b;
             }
-        }
-        return res;
+            return mp[a] < mp[b];
+        });
+        return nums;
     }
 };
