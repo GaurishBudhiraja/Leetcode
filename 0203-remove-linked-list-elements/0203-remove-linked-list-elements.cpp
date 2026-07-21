014bbea08348d12cc0a058115aae27d3964ab4a9
Gaurish Budhiraja <gaurishbudhiraja2005@gmail.com>
2026-07-21 12:44:52 +0530

    Time: 0 ms (100%), Space: 20.6 MB (5.33%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (!head) return nullptr;
+        head->next = removeElements(head->next, val);
+        return head->val == val ? head->next : head;
+    }
+};
