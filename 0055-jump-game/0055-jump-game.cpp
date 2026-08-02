832466f86e5595023cdb5c239ccd2918f59c0c64
Gaurish Budhiraja <gaurishbudhiraja2005@gmail.com>
2026-08-02 20:58:41 +0530

    Time: 0 ms (100%), Space: 52.5 MB (16.43%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>c) return false;
+            c=max(c,i+nums[i]);
+        }
+        return true;
+    }
+};
