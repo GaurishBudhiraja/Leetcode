3d033a15b5526a68413550a23eb5d00b982fee41
Gaurish Budhiraja <gaurishbudhiraja2005@gmail.com>
2026-08-17 09:40:20 +0530

    Time: 961 ms (13.79%), Space: 27.7 MB (51.81%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -7,22 +7,22 @@ public:
             pre[i+1]=pre[i]+s[i];
         }
         vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int len=2;len<=n;len++){
-            for(int l=0;l+len<=n;l++){
-                int r=l+len-1;
-                for(int k=l;k<r;k++){
-                    int left = pre[k+1]-pre[l];
+        for(int i=2;i<=n;i++){
+            for(int j=0;j+i<=n;j++){
+                int r=j+i-1;
+                for(int k=j;k<r;k++){
+                    int left = pre[k+1]-pre[j];
                     int right = pre[r+1]-pre[k+1];
                     if(left<right){
-                        dp[l][r] = max(dp[l][r],left + dp[l][k]);
-                    }else if(right < left){
-                        dp[l][r] = max(dp[l][r],right + dp[k + 1][r]);
+                        dp[j][r] = max(dp[j][r],left + dp[j][k]);
+                    }else if(right<left){
+                        dp[j][r] = max(dp[j][r],right+dp[k+1][r]);
                     }else{
-                        dp[l][r] = max(dp[l][r],left + max(dp[l][k],dp[k + 1][r]));
+                        dp[j][r] = max(dp[j][r],left+max(dp[j][k],dp[k+1][r]));
                     }
                 }
             }
         }
-        return dp[0][n - 1];
+        return dp[0][n-1];
     }
 };
