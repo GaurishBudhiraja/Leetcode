02cf0781bbff2b0b38e61232cbdb6986f3c6a2e1
Gaurish Budhiraja <gaurishbudhiraja2005@gmail.com>
2026-08-21 09:31:41 +0530

    Time: 0 ms (100%), Space: 12.7 MB (84%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,9 +3,9 @@ public:
     int minimumTotal(vector<vector<int>>& t) {
         for(int i=t.size()-2;i>=0;i--){
             for(int j=0;j<t[i].size();j++){
-                int below = t[i+1][j];
-                int belowRight = t[i+1][j+1];
-                t[i][j]+=min(below,belowRight);
+                int l=t[i+1][j];
+                int r=t[i+1][j+1];
+                t[i][j]+=min(l,r);
             }
         }
         return t[0][0];
